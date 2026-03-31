b5f7e79182a122c4da9be0fe598e1c4478d14687
itaha2005 <jamiltaha2005@gmail.com>
2026-03-31 16:26:58 +0500

    Add files via upload

---
+++ utils/color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class MyAppColors {
+  static const Color primaryColors = Color.fromARGB(255, 248, 247, 248);
+  static const Color secondaryColors = Color.fromARGB(255, 0, 0, 0);
+  static const Color tertiaryColors = Color.fromARGB(255, 112, 221, 248);
+}
---
+++ utils/string.dart
@@ -0,0 +1,15 @@
+class MyAppStrings {
+  static const String loginText = "Login";
+
+  static const String username = "Enter your Username";
+
+  static const String ID = "Enter your Student ID";
+  static const String password = "Enter your Password";
+  static const String confirmPassword = "Confirm Password";
+  static const String email = "Email";
+  static const String firstLastname = "First & Last Name";
+  static const String UMTStudentID = "UMT Student ID";
+  static const String code = "Code";
+
+  static const String analyticstools = "Analytics Tools";
+}
